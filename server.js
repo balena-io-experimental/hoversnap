8794d766d858c5655a708ee9aabc286d37cdfb74
Petros Angelatos <petrosagg@gmail.com>
2014-06-20 20:09:38 +0100

    Reimplement using streams and filtering

--- server.js
+++ server.js
@@ -1,30 +1,64 @@
-var Gpio = require('onoff').Gpio
-var pad = new Gpio(17, 'in', 'both')
+var _ = require('lodash');
+var es = require('event-stream');
+var Gpio = require('onoff').Gpio;
 
+// Create a raw event stream from the Gpio
+var padEventStream = es.through();
+
+var pad = new Gpio(17, 'in', 'both');
+pad.watch(function(err, value) {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	padEventStream.write(value);
+});
+
+// Deduplication filter
+var dedupFilter = function() {
+	var prev = null;
+
+	return function(data) {
+		if (data !== prev) {
+			this.emit('data', data);
+			prev = data;
+		}
+	}
+};
+
+// Implement a low-pass filter
+var lowPassFilter = function(wait) {
+	var emit = _.debounce(function(data) {
+		this.emit('data', data);
+	}, wait, {leading: true});
+
+	return function(data) {
+		emit.call(this, data);
+	}
+};
+
+// Implement a threshold filter
+var thresholdFilter = function(threshold) {
+	return function(data) {
+		if (data > threshold) {
+			this.emit('data', data);
+		}
+	}
+};
+
+// Helper that captures the picture
 var spawn = require('child_process').spawn;
 var ps = spawn('gphoto2', ['--shell']);
 
-ps.stdout.pipe(process.stdout);
-ps.stderr.pipe(process.stderr);
+var captureImage = function() {
+	ps.stdin.write('capture-image\n');
+	console.log('Picture captured!');
+};
 
-var padDown = 0;
-var timeout = 0;
-
-pad.watch(function(err, value) {
-	if (value === 0 && padDown === 0) {
-		padDown = Date.now()
-		console.log('Stay there for at least 3 seconds...');
-		timeout = setTimeout(function() {
-			console.log('Ready! Waiting for jump...');
-		}, 3000)
-	} else if (padDown !== 0) {
-		if (Date.now() - padDown < 3000) {
-			console.log('Jump before 3 seconds detected. Try again.');
-			clearTimeout(timeout);
-		} else {
-			ps.stdin.write('capture-image\n');
-			console.log('Jump detected! Picture captured!');
-		}
-		padDown = 0;
-	}
-});
+es.pipeline(
+	padEventStream,
+	es.through(dedupFilter()),
+	es.through(lowPassFilter(3000)),
+	es.through(thresholdFilter(0.5)),
+	es.mapSync(captureImage)
+);
