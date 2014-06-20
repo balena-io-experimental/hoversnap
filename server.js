332d77ff283aeae405b12445568f49842b64c942
Petros Angelatos <petrosagg@gmail.com>
2014-06-20 18:17:07 +0100

    Add logs

--- server.js
+++ server.js
@@ -8,15 +8,23 @@ ps.stdout.pipe(process.stdout);
 ps.stderr.pipe(process.stderr);
 
 var padDown = 0;
+var timeout = 0;
 
 pad.watch(function(err, value) {
 	if (value === 0 && padDown === 0) {
 		padDown = Date.now()
+		console.log('Stay there for at least 3 seconds...');
+		timeout = setTimeout(function() {
+			console.log('Ready! Waiting for jump...');
+		}, 3000)
 	} else {
 		if (Date.now() - padDown < 3000) {
+			console.log('Jump before 3 seconds detected. Try again.');
+			clearTimeout(timeout);
 			padDown = 0;
 		} else {
 			ps.stdin.write('capture-image\n');
+			console.log('Jump detected! Picture captured!');
 		}
 	}
 });
