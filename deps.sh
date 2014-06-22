63d3bfdaa527d09ea9bdeda40370cd9c7afbf137
Petros Angelatos <petrosagg@gmail.com>
2014-06-22 16:44:06 +0100

    Install quick2wire-gpio-admin

--- deps.sh
+++ deps.sh
@@ -1,2 +1,8 @@
+# Install gphoto2 used to control the camera
 apt-get update
 apt-get install -y gphoto2
+
+# Install quick2wire-gpio-admin needed by pi-gpio
+git clone --depth=1 git://github.com/quick2wire/quick2wire-gpio-admin.git
+cd quick2wire-gpio-admin
+make && make install
